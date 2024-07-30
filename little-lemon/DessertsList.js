b5f85f5c0fa807cd03a3f15f6951836967881cd3
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-07-29 23:01:56 -0400

    improvement and chain

--- little-lemon/DessertsList.js
+++ little-lemon/DessertsList.js
@@ -1,15 +1,15 @@
 function DessertsList(props) {
   // Implement the component here.
-  let menu = props.data
-    .filter((desert) => desert.calories < 500)
-    .sort((a, b) => a.calories - b.calories);
   return (
     <ul>
-      {menu.map((dessert) => (
-        <li key={dessert.name}>
-          {dessert.name} - {dessert.calories} cal
-        </li>
-      ))}
+      {props.data
+        .filter((desert) => desert.calories < 500)
+        .sort((a, b) => a.calories - b.calories)
+        .map((dessert) => (
+          <li key={dessert.name}>
+            {dessert.name} - {dessert.calories} cal
+          </li>
+        ))}
     </ul>
   );
 }
